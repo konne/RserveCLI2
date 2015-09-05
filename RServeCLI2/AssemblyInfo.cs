ffc656023188e504a06bf88063d6da332f858f05
Suraj K. Gupta <surajg@gmail.com>
2015-09-04 21:25:59 -0400

    more AssemblyInfo cleanup

--- RServeCLI2/AssemblyInfo.cs
+++ RServeCLI2/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 // All rights reserved.
 //-----------------------------------------------------------------------
 
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -18,14 +19,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 [assembly: InternalsVisibleTo( "RserveCLI2.Test" )]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion( "1.0.0.0" )]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-// [assembly: AssemblyDelaySign(false)]
-// [assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
